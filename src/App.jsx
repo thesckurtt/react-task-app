afcee36cf644f0607a3a13f82c61407f320bed39
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-03-30 20:34:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CardsSection from "./components/CardsSection";
 import InputSearch from "./components/InputSearch";
 import TaskCard from "./components/TaskCard";
+import MenuOptions from "./components/MenuOptions";
 
 function App() {
   const [tasks, setTask] = useState([{title: "Tarefa exemplo", date: "2025-03-06", currentDate: "30/02"}]);
@@ -16,6 +17,7 @@ function App() {
   return (
     <>
       <InputSearch onAddTask={addTask}/>
+      <MenuOptions />
       <CardsSection data={tasks} />
     </>
   );
