88f132fe5e1e9d81766f4ffb579eb28d2ec40941
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-03-30 21:05:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import CardsSection from "./components/CardsSection";
 import InputSearch from "./components/InputSearch";
-import TaskCard from "./components/TaskCard";
 import MenuOptions from "./components/MenuOptions";
 
 function App() {
-  const [tasks, setTask] = useState([{title: "Tarefa exemplo", date: "2025-03-06", currentDate: "30/02"}]);
+  const [tasks, setTask] = useState([{title: "Tarefa exemplo", date: "2025-03-06", currentDate: "30/02", uuid: "32560819-050d-4e79-b5e9-b4b4ea211a96"}]);
 
   console.log(tasks);
 
