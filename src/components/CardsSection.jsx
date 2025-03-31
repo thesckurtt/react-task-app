bd971ee6110ddc2cd0103af807a29a97e756cd50
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-03-30 21:05:42 -0300

    Update CardsSection.jsx

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -3,8 +3,8 @@ import TaskCard from "./TaskCard";
 const CardsSection = ({ data }) => {
   return (
     <section class="cards-section d-flex flex-column align-items-center">
-      {data.reverse().map(({ title, date, currentDate }, index) => (
-        <TaskCard key={index} title={title} date={date} currentDate={currentDate} />
+      {data.reverse().map(({ title, date, currentDate, uuid }) => (
+        <TaskCard key={uuid} title={title} date={date} currentDate={currentDate} uuid={uuid}/>
       ))}
     </section>
   );
