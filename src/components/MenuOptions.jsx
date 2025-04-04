9a7fba3a4ec87c39428a27590eeeb22141bbd61d
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-04 15:47:24 -0300

    Update MenuOptions.jsx

--- src/components/MenuOptions.jsx
+++ src/components/MenuOptions.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const MenuOptions = ({deleteTask}) => {
+const MenuOptions = ({deleteTask, tasks}) => {
+  console.log('Esse log é do MenuOptions', tasks)
   return (
     <section className="info-section limit d-flex align-items-center justify-content-between">
       <span className="text-center h4 fw-bold">Gerencie suas tarefas</span>
       <div>
-        <button className="btn btn-danger" onClick={()=> deleteTask({deleteAll: true})}>Apagar Todas</button>
+        <button className="btn btn-danger" onClick={()=> deleteTask({deleteAll: true})} disabled={tasks.length === 0}>Apagar Todas</button>
       </div>
     </section>
   );
