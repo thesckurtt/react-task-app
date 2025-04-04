5e0714c8a0e1b77cb5b81ba6cd78219270a74436
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-04 15:36:50 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,22 @@ import Swal from "sweetalert2";
 function App() {
   const [tasks, setTasks] = useState([
     {
-      title: "Tarefa exemplo",
+      title: "Tarefa exemplo 1",
       date: "2025-03-06",
       currentDate: "30/02",
       uuid: "32560819-050d-4e79-b5e9-b4b4ea211a96",
+    },
+    {
+      title: "Tarefa exemplo 2",
+      date: "2025-03-06",
+      currentDate: "30/02",
+      uuid: "32560819-050d-4e79-b5e9-b4b4ea394a96",
+    },
+    {
+      title: "Tarefa exemplo 3",
+      date: "2025-03-06",
+      currentDate: "30/02",
+      uuid: "32569465-050d-4e79-b5e9-b4b4ea394a96",
     },
   ]);
 
@@ -24,41 +36,54 @@ function App() {
     setTasks([...tasks, task]);
   };
 
-  const deleteTask = (id, deleteAll = false) => {
-    const task = tasks.filter(e => e.uuid === id)[0];
+  const deleteTask = ({ id = "", deleteAll = false }) => {
+    if (id) {
+      const task = tasks.filter((e) => e.uuid === id)[0];
 
-    Swal.fire({
-      title: "Você tem certeza?",
-      icon: "warning",
-      html: `Você está prestes a deletar a tarefa: <b>${(task.title).toString()}</b>`,
-      showCancelButton: true,
-      confirmButtonText: "Sim, deletar!",
-      cancelButtonColor: "#d33",
-      cancelButtonText: "Cancelar",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Tarefa Excluída com sucesso!",
-          text: `A tarefa de id [${id}] foi excluída.`,
-          icon: "success",
-        });
-        setTasks(tasks.filter((e) => e.uuid !== id));
-      }
-    });
+      Swal.fire({
+        title: "Você tem certeza?",
+        icon: "warning",
+        html: `Você está prestes a deletar a tarefa: <b>${task.title.toString()}</b>`,
+        showCancelButton: true,
+        confirmButtonText: "Sim, deletar!",
+        cancelButtonColor: "#d33",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Tarefa Excluída com sucesso!",
+            text: `A tarefa de id [${id}] foi excluída.`,
+            icon: "success",
+          });
+          setTasks(tasks.filter((e) => e.uuid !== id));
+        }
+      });
+    }
 
     // Implementar deleteAll
-    // if (deleteAll) {
-    //   setTasks([]);
-    // }
+    if (deleteAll) {
+      Swal.fire({
+        title: "Tem certeza que deseja apagar todas as tarefas?",
+        icon: "warning",
+        html: `Você está prestes a apagar <b>${tasks.length}</b> tarefas, tem certeza disso?`,
+        showCancelButton: true,
+        confirmButtonText: "Sim, deletar!",
+        cancelButtonColor: "#d33",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          setTasks([]);
+        }
+      });
+    }
 
     // setTasks(tasks.filter((e) => e.uuid !== id));
   };
 
-  // deleteTasks('32560819-050d-4e79-b5e9-b4b4ea211a96');
   return (
     <>
       <InputSearch onAddTask={addTask} />
-      <MenuOptions />
+      <MenuOptions deleteTask={deleteTask} />
       <CardsSection deleteTask={deleteTask} data={tasks} />
     </>
   );
